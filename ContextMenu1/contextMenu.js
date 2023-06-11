692462d496d53252b6f0f1bf748fd1abd0df71b9
Avraham Zilberman <az@oodidactics.com>
2023-06-11 12:51:07 +0300

    Add Mode

--- ContextMenu1/contextMenu.js
+++ ContextMenu1/contextMenu.js
@@ -1,3 +1,40 @@
+const bigShotUrl = "https://bigshotchallenge.bigshot-station.com/dark/symbol?symbol="
+const tradingViewUrl = "https://il.tradingview.com/chart/DdugIrMT/?symbol="
+let menuOptions = {
+  "bigShotOpenTab_menuItem":(info,tab)=>Menu_OpenTab(info,tab),
+  "bigShotClearTabs_menuItem":(info,tab)=>Menu_StartTrade(info)
+}
+let modes = {
+                challange:"https://bigshotchallenge.bigshot-station.com/dark/symbol?symbol=",
+                demo:"https://demostocks.bigshot-station.com//dark/symbol?symbol="
+}
+currentMode = "challange"
+function getBigshotURL(){
+  return modes[currentMode]
+}
+let initialTabs = 
+
+[
+  "https://www.beithanoar.org.il/%D7%A4%D7%A2%D7%99%D7%9C%D7%95%D7%99%D7%95%D7%AA/%D7%91%D7%A8%D7%99%D7%9B%D7%94-%D7%95%D7%9E%D7%95%D7%A2%D7%93%D7%95%D7%9F-%D7%94%D7%9B%D7%95%D7%A9%D7%A8/%D7%A9%D7%A2%D7%95%D7%AA-%D7%A4%D7%A2%D7%99%D7%9C%D7%95%D7%AA-%D7%94%D7%91%D7%A8%D7%99%D7%9B%D7%94",
+  "https://www.nasdaq.com/market-activity/stocks/spy/latest-real-time-trades",
+  "https://calendar.google.com/calendar/u/0/r",
+  "https://finance.yahoo.com/most-active/?offset=0&count=250",
+   "https://web.whatsapp.com/",
+"https://marketchameleon.com/Calendar/Earnings" ,
+"https://docs.google.com/spreadsheets/d/13mqqtReJnJkSpuTiCa_TSJs8-kBLsXd4SqjKZCGBIhM/edit#gid=0",
+"https://finviz.com/map.ashx",
+"https://bigshotchallenge.bigshot-station.com/sessions/new",
+"https://bigshotchallenge.bigshot-station.com/dark/screener",
+]
+let initialSymbols = ["SPY"]
+
+function changeMode(mode){
+  currentMode = mode 
+}
+
+
+
+
 function init(){
   var clearStorage = true 
   if (clearStorage){
@@ -5,33 +42,96 @@ function init(){
       chrome.storage.local.set({"watchList":[]})
   }
 }
-function openSymbol(symbol){
-  let url = "https://bigshotchallenge.bigshot-station.com/dark/symbol?symbol="+symbol
-      
-  chrome.tabs.create({ url: url},(tab)=>
+function openTab(url,pinned){
+  chrome.tabs.create({ url: url,pinned:pinned},(tab)=>
   {
     
     chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, updatedTab) {
         if (tabId === tab.id && changeInfo.status === "complete") {
-            //chrome.tabs.executeScript(tabId, {code: "document.title = 'New Title'"});
-            chrome.tabs.sendMessage(tabId,symbol)
-            
-            var obj = chrome.storage.local.get("watchList").then((result)=>
-            {
-              
-                result ={...result,watchList:[...result.watchList,linkText]}
-              chrome.storage.local.set(result)
-              chrome.tabs.sendMessage(tabId,result)
-              
-            }
-            )
-            
+            console.log("LOADED")
         }
-      });
+            //chrome.tabs.executeScript(tabId, {code: "document.title = ')
+  })
+  })
+}
+function isSymbolTab(updatedTab){
+    return updatedTab.url.startsWith(getBigshotURL())
+        
     
-  });
-  // Get the current tab
+    
+}
+function getSymbol(updatedTab){
+  let retval = updatedTab.url.replace(getBigshotURL(),"")
+  console.log("UpdatedTAB===>>>",updatedTab)
+  return retval 
+}
+chrome.tabs.onUpdated.addListener((tabId,changeInfo,updatedTab)=>{
+    console.log("CHANGE INFO= >>>>>",changeInfo)
+    if (isSymbolTab(updatedTab)){
+      let symbol = getSymbol(updatedTab)
+      chrome.tabs.sendMessage(tabId,symbol)
+    }
+})
+function activateTab(url,pinned){
+  let func = (posType)=>
+  chrome.tabs.query({url:url+"&"+posType},(tabs)=>{  
+    if (tabs.length == 0 ){
+      chrome.tabs.create({ url: url+"&"+posType,pinned:pinned})
+    }
+    else
+      chrome.tabs.update(tabs[0].id,{active:true})
+    
+
+  
  
+  }) 
+  func(currentMode)
+  
+
+}
+
+function openSymbol(symbol,pinned){
+  let url = getBigshotURL()+symbol
+  activateTab(url,pinned)
+  
+ 
+ 
+}
+function Menu_OpenTab(info,tab){
+//  var target = chrome.contextMenus.getTargetElement(info, tab);
+      let linkText = ''
+      if(info.linkUrl)
+      {
+        let   a = info.linkUrl.split("/")
+        linkText = a[a.length-2]
+      }
+      if (linkText == '')
+        linkText = info.selectionText 
+      openSymbol(linkText,false)
+      
+}
+function Menu_StartTrade(info){
+  
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
+    // Loop through all tabs and close them
+    for (var i = 0; i < tabs.length; i++) {
+      chrome.tabs.remove(tabs[i].id);
+
+    }
+    
+    //chrome.tabs.update(tabs[tabs.length - 2].id, {url: "https://bigshotchallenge.bigshot-station.com/sessions/new"});
+   
+    initialTabs.map(url=> openTab(url,true) )
+    initialSymbols.map(url=>openSymbol(url,true))
+    
+
+    
+
+    
+  
+  
+  
+  });
 }
 chrome.commands.onCommand.addListener(function(command) {
   console.log("COMMAND",command)
@@ -62,52 +162,23 @@ chrome.runtime.onInstalled.addListener(function() {
         openSymbol(request.openSymbol)
 
     }
+    if (request.getMode){
+        console.log("getMode")
+        chrome.runtime.sendMessage({mode:currentMode})
+    }
+    if (request.toggleMode){
+      
+      changeMode(request.toggleMode)
+    }
+    if (request.startTrade){
+      Menu_StartTrade(undefined)
+    }
+    if (request.ActivateTab){
+        activateTab(request.ActivateTab)
+    }
   })
   chrome.contextMenus.onClicked.addListener(function(info, tab) {
- 
-    if (info.menuItemId == "bigShotOpenTab_menuItem") {
-      let   a = info.linkUrl.split("/")
-      let linkText = a[a.length-2]
-      if (linkText == '')
-        linkText = info.selectionText 
-      let url = "https://bigshotchallenge.bigshot-station.com/dark/symbol?symbol="+linkText
-      
-      chrome.tabs.create({ url: url},(tab)=>
-      {
-        
-        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, updatedTab) {
-            if (tabId === tab.id && changeInfo.status === "complete") {
-                //chrome.tabs.executeScript(tabId, {code: "document.title = 'New Title'"});
-                chrome.tabs.sendMessage(tabId,linkText)
-                
-                var obj = chrome.storage.local.get("watchList").then((result)=>
-                {
-                  
-                    result ={...result,watchList:[...result.watchList,linkText]}
-                  chrome.storage.local.set(result)
-                  chrome.tabs.sendMessage(tabId,result)
-                  
-                }
-                )
-                
-            }
-          });
-        
-      });
-      // Get the current tab
-      
-      
-    }
-    if (info.menuItemId == "bigShotClearTabs_menuItem"){
-        chrome.tabs.query({currentWindow: true}, function(tabs) {
-            // Loop through all tabs and close them
-            for (var i = 0; i < tabs.length-1; i++) {
-              chrome.tabs.remove(tabs[i].id);
-
-            }
-            chrome.tabs.update(tabs[tabs.length - 1].id, {url: "https://bigshotchallenge.bigshot-station.com/sessions/new"});
-            chrome.tabs.update(tab.id, {active: true});
-          });
-    }
+    menuOptions[info.menuItemId](info,tab)
   });
+  
   
--- ContextMenu1/popup.js
+++ ContextMenu1/popup.js
@@ -2,8 +2,14 @@ function openSymbol(value){
     
     chrome.runtime.sendMessage({openSymbol:value})  
 }
+function startTrade(){
+    chrome.runtime.sendMessage({startTrade:true}) 
+}
 document.addEventListener("DOMContentLoaded",()=>{
-    var btn = document.getElementById("btn")
+    var btn = document.getElementById("btnStartTrade")
+    var btn2 = document.getElementById("btnDemoMode")
+    var btn3 = document.getElementById("btnChallangeMode")
+
     var symbol = document.getElementById("symbol")
     symbol.focus()
     symbol.addEventListener("keypress",(e)=>{
@@ -12,8 +18,29 @@ document.addEventListener("DOMContentLoaded",()=>{
         }
     }
     )
-    btn.addEventListener("click",(evt)=>openSymbol(document.getElementById("symbol").value))
-
+    
+    btn.addEventListener("click",(evt)=>startTrade())
+    btn2.addEventListener("click",(evt)=> chrome.runtime.sendMessage({toggleMode:DEMO})  )
+    btn3.addEventListener("click",(evt)=> chrome.runtime.sendMessage({toggleMode:CHALLANGE})  )
+    
     
 }
 )
+const CHALLANGE = "challange"
+const DEMO = "demo"
+
+let mode = CHALLANGE
+function toggleMode(mode){
+    
+  
+
+    chrome.runtime.sendMessage({toggleMode:mode})  
+
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.mode)
+            document.getElementById("btnToggleMode").innerHTML = request.mode
+
+})
+
